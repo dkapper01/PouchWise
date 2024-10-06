ea93dec2ce3429cc6de4efb5cd1071fc288847b8
Daniel Kapper <dkapper01@gmail.com>
2024-10-06 19:34:52 -0400

    remove deploy button

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,9 +38,6 @@ export default function RootLayout({
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
