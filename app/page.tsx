12d6c2e16f893d9691ae07cde5d9e645bac10a75
Daniel Kapper <dkapper01@gmail.com>
2024-10-08 21:08:52 -0400

    remove code

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
-import { Hero } from "@/components/hero";
-import Walkthrough from "@/hooks/user-walkthrough";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
 export default async function Index() {
   return (
     <>
-      {/* <Hero /> */}
-      <Walkthrough />
       <main className="bg-green-500"></main>
     </>
   );
