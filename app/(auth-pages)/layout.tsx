1cdc0ac14db40cf0767df1c71ca564099454b868
Daniel Kapper <dkapper01@gmail.com>
2024-10-09 20:47:38 -0400

    fix page breaking issue

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,7 +1,14 @@
+// import BottomNavBar from "@/components/BottomNavBar";
+
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="flex flex-col gap-12 items-center">{children}</div>;
+  return (
+    <div className="flex flex-col gap-12 items-center">
+      {children}
+      {/* <BottomNavBar /> */}
+    </div>
+  );
 }
